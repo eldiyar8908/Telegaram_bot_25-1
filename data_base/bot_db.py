fda1bf1440cb500ef91b01b7960441ddace1ea0e
eldiyar8908 <daniaroveldiyar@gmail.com>
2023-01-25 20:40:17 +0600

    'Telegram_bot_25-1'

--- data_base/bot_db.py
+++ data_base/bot_db.py
@@ -1,4 +1,7 @@
 import sqlite3
+import random
+from handlers.config import bot
+from aiogram import types
 
 
 def sql_create():
@@ -19,15 +22,26 @@ def sql_create():
 
 
 
-# async def sql_command_insert(state):
-#     async with state.proxy() as data:
-#         cursor.execute("INSERT INTO mentors "
-#                        "(name, direction, age, gruppa) VALUES "
-#                        "(?, ?, ?, ?)", tuple(data.values()))
-#         db.commit()
-
-
 async def sql_command_insert(state):
     async with state.proxy() as data:
         cursor.execute("INSERT INTO mentors VALUES (?, ?, ?, ?,?)", tuple(data.values()))
         db.commit()
+
+
+async def sql_command_random(message: types.Message):
+    result = cursor.execute('SELECT * FROM mentors').fetchall()
+    random_user = random.choice(result)
+    await bot.send_message(
+        message.from_user.id,
+        random_user[-1],
+        text=f'{random_user[1]}, {random_user[2]}, {random_user[3]}, {random_user[4]},'
+    )
+
+
+async def sql_command_all():
+    return cursor.execute('SELECT * FROM mentors').fetchall()
+
+
+async def sql_command_delete(user_id):
+    cursor.execute('DELETE FROM mentors WHERE id = ?', (user_id,))
+    db.commit()
