ac92caf829ed3e871c010687988a30591f73ceda
eldiyar8908 <daniaroveldiyar@gmail.com>
2023-01-25 21:34:56 +0600

    'Telegram_bot_25-1'

---
+++ handlers/notification.py
@@ -0,0 +1,29 @@
+import aioschedule
+import asyncio
+from aiogram import types, Dispatcher
+from handlers.config import bot
+
+
+async def get_chat_id(message: types.Message):
+    global chat_id
+    chat_id = []
+    chat_id.append(message.from_user.id)
+    await message.answer('ok')
+
+
+async def last_day():
+    for id in chat_id:
+        await bot.send_message(id, 'Завтра выходной.')
+
+
+async def scheduler():
+    aioschedule.every().day.at('21:33').do(last_day)
+    while 1:
+        await aioschedule.run_pending()
+        await asyncio.sleep(60)
+
+
+
+
+def register_message_notification(dp: Dispatcher):
+    dp.register_message_handler(get_chat_id, lambda word: 'напомни' in word.text)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import asyncio
+
 from aiogram.utils import executor
 import logging
-from handlers import client, extra, call_back, fsmAdminMentor
+from handlers import client, extra, call_back, fsmAdminMentor, notification
 from handlers.config import dp
 from data_base.bot_db import sql_create
 
 async def on_startup(_):
+    asyncio.create_task(notification.scheduler())
     sql_create()
 
-
+notification.register_message_notification(dp)
 fsmAdminMentor.register_handlers_anketa(dp)
 call_back.register_handlers_callback(dp)
 client.register_handlers_client(dp)
